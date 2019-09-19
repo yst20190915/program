9f971c1ccb69201b50243c54ffd129e94aece15a
Administrator <Administrator@PC-20190915MQBD>
2019-09-19 14:10:46 +0800

    Bugs Bunny

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var name = "Bugs Bunny";
+            var age = 25;
+            var isRabbit = true;
+            Type nameType = name.GetType();
+            Type ageType = age.GetType();
+            Type isRabbitType = isRabbit.GetType();
+            Console.WriteLine("name is type " + nameType.ToString());
+            Console.WriteLine("age is type " + ageType.ToString());
+            Console.WriteLine("israbbit is type " + isRabbitType.ToString());
+
         }
     }
 }
