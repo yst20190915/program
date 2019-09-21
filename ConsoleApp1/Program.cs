55ff31d9e77653eb6a01a9978b728c45287d701b
Administrator <Administrator@PC-20190915MQBD>
2019-09-21 21:26:11 +0800

    goto Found

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,13 +7,26 @@ namespace week
     {    
         static void Main(string [] ags)
         {
-            for (int j=0;j<20;j++)
+            Console.WriteLine("请输入要查找的文字:");
+            string inputstr = Console.ReadLine();
+            string[] mystr = new string[3];
+            mystr[0] = "你";
+            mystr[1] = "好";
+            mystr[2] = "呀";
+            for(int i=0;i<mystr.Length;i++)
             {
-                if ((j % 2) == 0)
-                    continue;
-                Console.WriteLine(j);
+                if (mystr [i].Equals(inputstr))
+                {
+                    goto Found;
+                }
+                 
             }
-            
+            Console.WriteLine("您查找的{0}不存在", inputstr);
+            goto Finish;
+        Found:
+            Console.WriteLine("您查找的{0}存在", inputstr);
+        Finish:
+            Console.WriteLine("查找完毕");
         }
     }
 }
