18920a896e1051eeac9b21979c6ff606d06f5419
Administrator <Administrator@PC-20190915MQBD>
2019-09-23 16:37:38 +0800

    GenericCustomer customer = new Nevermore60Customer("Jack","Jone");

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,22 +5,28 @@ namespace Worx
     abstract class GenericCustomer//抽象函数
     {
         private string name;
-        public GenericCustomer ()
+        public  GenericCustomer (string name)
         {
-            name = "<no name>";
+            this.name = name;
         }
        
     }
     class Nevermore60Customer:GenericCustomer //派生类
     {
-        private int highCostMinutesUsed;
+        public Nevermore60Customer (string name,string referrerName)
+            :base (name )
+        {
+            this.referrerName = referrerName;
+        }
+        private string referrerName;
+        private uint highCostMinutesUsed;
     }
 
     class A
     {
         static  void Main()
         {
-            GenericCustomer customer = new Nevermore60Customer();
+            GenericCustomer customer = new Nevermore60Customer("Jack","Jone");
 
 
             
