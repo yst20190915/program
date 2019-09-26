877c9d04ebc1f8ad5de195e431495e6323a1a2b7
Administrator <Administrator@PC-20190915MQBD>
2019-09-26 10:43:30 +0800

    var list = new ArrayList();

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,52 +1,24 @@
 ﻿using System;
+using System.Collections;
 
-
-namespace Worx.ProCSharp
+namespace Wrox.ProCSharp
 {
-    
-    public interface IBankAccount//接口
+       public  class A
     {
-        void PayIn(decimal amount);
-        bool Withdraw(decimal amount);
-        decimal Balance { get; }
-       
-    }   
-}
-
-namespace Worx.ProCSharp .VenusBank
-{
-    public class SaverAccount:IBankAccount
-    {
-        private decimal balance;
-        public void PayIn(decimal amount)
+          public static void Main()
+       {
+         var list = new ArrayList();
+        list.Add(44);
+        int i1 = (int)list[0];
+        foreach (int i2 in list )
         {
-            balance += amount;
-        }
-        public bool Withdraw(decimal amount)
-        {
-            if (balance >=amount )
-            {
-                balance -= amount;
-                return true;
-            }
-            Console.WriteLine("Withdrawal attempt failed.");
-            return false;
-        }
-        public decimal Balance
-        {
-            get
-            {
-                return balance;
-            }
-        }
-        public override string ToString()
-        {
-            return String.Format("Venus Bank Saver:Balance=(0,6:C)", balance);
-        }
-        public static void Main()
-        {
-            string account = Console.ReadLine();
-            
+            Console.WriteLine(i2);
         }
     }
+    }
+
+   
+  
 }
+
+
